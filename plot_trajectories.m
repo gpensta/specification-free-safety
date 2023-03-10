d3d3fe78a04ee5a4d474b7495697603e2106579a
gpensta <gwendal.priser@ensta-paris.fr>
2023-03-10 15:58:39 +0100

    Update.

--- plot_trajectories.m
+++ plot_trajectories.m
@@ -12,16 +12,16 @@ eta = 0; % noise on the policy: a percentage of the max interval.
 horizon = 25; 
 
 % END PARAMS
-
-
 n_points = 60;
 cc = hsv(n_points);
 points = [2 * rand(1, n_points) - 1; 4 * rand(1, n_points) - 2.]; % initial conditions.
+
 T = [0; 0]; 
 U = max_u * zonotope(interval([0.; -1.],[0. ; 1.]));
 B = k_step_backward(T, U, A, dt, k2);
 Br = 1 * B;
 close all;
+
 
 figure;
 hold on;
@@ -30,6 +30,7 @@ ylim([-2 2]);
 
 plot(B, [1,2], 'k', 'linewidth', 2.); % Plot backward set
 plot(Br, [1,2], 'k--', 'linewidth', .5); % Plot backward set
+% plot([-1:.1:1], -6 - 11 * [-1:.1:1])
 
 for i = 1:n_points
     x = points(:, i);
@@ -37,6 +38,7 @@ for i = 1:n_points
     traj(:, 1) = x;
     col = [.9 .9 .9];
     super = 0;
+    had_super = 0;
     for t = 1:horizon
         if mod(t, k1) == 0
             W = get_max_w_quick(x, Br, U, A, dt);
@@ -46,6 +48,7 @@ for i = 1:n_points
              [u, super] = supervision(u, W);
          end
          if super
+            had_super = 1; 
             col = [0. .5 0.];
             scatter(x(1, 1), x(2, 1), 80, 'r', '*'); 
          end
@@ -53,8 +56,22 @@ for i = 1:n_points
         x = move(x, u, A, dt);
         traj(:, t+1) = x;  
     end
+    cols = [0.9 .9 0.9; 0.5 0. 0.;  0.93 0.69 0.13];
+    if abs(traj(1, horizon)) > 0.5
+        c = 2;
+    else
+        c = 1;
+    end
+    
+    if had_super
+        
+        s = 100;
+        c = 3;
+    else
+        s = 25;
+    end
     plot(traj(1, :), traj(2, :),'color', col, 'linewidth', 1.);
-    scatter(traj(1, 1), traj(2, 1),[], col, '>', 'filled'); 
+    scatter(traj(1, 1), traj(2, 1),s, cols(c, :), 'o', 'filled'); 
 end 
  
 % Plot contour 
@@ -80,12 +97,13 @@ set(gca,'Xtick',[])
 function [res, super] = supervision(u, w)
     sup = w.sup;
     low = w.inf;
+    range = sup - low;
     super = 0;
     if u < low
-        u = low;
+        u = low; %+ .5 * range;
         super = 1;
     elseif u > sup
-        u = sup;
+        u = sup; % - .5 * range;
         super = 1; 
     end
     res = u;
@@ -150,4 +168,8 @@ end
 
 function res = move(x, u, A, dt)
         res = A*x + [0; dt]*u;
+end
+
+function y = map(x, a, b, c, d)
+    y = c + ((d - c)*(x-a))/(b -a);
 end
